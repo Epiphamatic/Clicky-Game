5372f4b06e1b022cfe819b0e38bd9128c725faea
Tri Pham <tjpham25@gmail.com>
2019-01-19 22:22:33 -0800

    Updates

---
+++ precache-manifest.b0aadb5f2a0f4b9d8a4f624f05a75c35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "16e5d385f0c131c44ced",
+    "url": "/Clicky-Game/static/js/main.16e5d385.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Clicky-Game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "16e5d385f0c131c44ced",
+    "url": "/Clicky-Game/static/css/main.7caff37e.chunk.css"
+  },
+  {
+    "revision": "6ffe10715255a35fa5a3c984d83674bd",
+    "url": "/Clicky-Game/index.html"
+  }
+];
